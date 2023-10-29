ae0380f30fe8cb82c2dd22ad8f1af00f9730946e
kit4py <105520646+kit4py@users.noreply.github.com>
2023-10-29 14:06:32 +0100

    Add files via upload

--- dockerbash.sh
+++ dockerbash.sh
@@ -21,9 +21,8 @@ python3 moveconf.py
 echo "INSTALLING ZSH AND POWERSHELL"
 echo ""
 apt install zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-python3 newtheme.py
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 zsh 
+echo "INSTALLATION COMPLETED!"
 
